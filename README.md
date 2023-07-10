fc12ec38918daf0a0f18516200820cf0a6e48d51
MikeZuck1 <105445747+MikeZuck1@users.noreply.github.com>
2023-07-11 00:03:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-higherOrderFunctions
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kohufk)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Import stylesheets
+import './style.css';
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML;
+
+
+const addTwo = num => {
+  return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 4;
+  let checkB = func(val);
+  return checkA === checkB ? func(val) : 'inconsistent results';  
+}
+
+console.log(checkConsistentOutput(addTwo, 10));
+// Output: inconsistent results
